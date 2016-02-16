24ea2f2c5e24bcf986f6adb8310091d898a0d422
Nick Hu <nick@511.pm>
2016-02-16 13:11:06 -0800

    move octopus init model code to the model

--- js/app.js
+++ js/app.js
@@ -13,6 +13,21 @@ $(function() {
 
   Cat.list = [];
   Cat.totalClickCnt = 0;
+  Cat.init = function() {
+    var names = ["Cat1","Cat2","Cat3","Cat4","Cat5",];
+    var imgUrls = [
+      "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426",
+      "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
+      "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
+      "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
+      "http://pngimg.com/upload/cat_PNG1631.png",
+    ];
+
+    for (var i = 0, l = names.length; i < l; i++){
+      var cat = new Cat(names[i]);
+      cat.imgSrc = imgUrls[i];
+    }
+  };
 
   Cat.prototype.generateId = function() {
     var id = Cat.list.length;
@@ -30,20 +45,7 @@ $(function() {
   //MV*
   var octopus = {
     init: function() {
-      var names = ["Cat1","Cat2","Cat3","Cat4","Cat5",];
-      var imgUrls = [
-        "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426",
-        "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
-        "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
-        "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
-        "http://pngimg.com/upload/cat_PNG1631.png",
-      ];
-
-      for (var i = 0, l = names.length; i < l; i++){
-        var cat = new Cat(names[i]);
-        cat.imgSrc = imgUrls[i];
-      }
-
+      Cat.init();
       view.init();
     },
 
