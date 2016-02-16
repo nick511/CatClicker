751ee7cf08f2074f4940a45123ff39c4c0d00326
Nick Hu <nick@511.pm>
2016-02-16 13:25:40 -0800

    *change model init data to json format *change cat's image to local

--- js/app.js
+++ js/app.js
@@ -14,18 +14,32 @@ $(function() {
   Cat.list = [];
   Cat.totalClickCnt = 0;
   Cat.init = function() {
-    var names = ["Cat1","Cat2","Cat3","Cat4","Cat5",];
-    var imgUrls = [
-      "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426",
-      "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
-      "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
-      "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
-      "http://pngimg.com/upload/cat_PNG1631.png",
+    var cats = [
+      {
+        name: "Cat1",
+        imgSrc: "img/cat1.jpg"
+      },
+      {
+        name: "Cat2",
+        imgSrc: "img/cat2.jpg"
+      },
+      {
+        name: "Cat3",
+        imgSrc: "img/cat3.jpg"
+      },
+      {
+        name: "Cat4",
+        imgSrc: "img/cat4.jpg"
+      },
+      {
+        name: "Cat5",
+        imgSrc: "img/cat5.png"
+      },
     ];
 
-    for (var i = 0, l = names.length; i < l; i++){
-      var cat = new Cat(names[i]);
-      cat.imgSrc = imgUrls[i];
+    for (var i = 0, l = cats.length; i < l; i++){
+      var cat = new Cat(cats[i].name);
+      cat.imgSrc = cats[i].imgSrc;
     }
   };
 
@@ -73,13 +87,14 @@ $(function() {
       view.catDiv = $("#catDiv");
       view.catName = $(".name", view.catDiv);
       view.catClickCnt = $(".clickCnt", view.catDiv);
+      view.catImgDiv = $(".catImgDiv", view.catDiv);
       view.catImg = $(".catImgDiv .catImg", view.catDiv);
       view.heart = $(".catImgDiv .heart", view.catDiv);
       
       view.totalClickCnt = $("#totalClickCnt");
 
       //set event
-      view.catImg.click(function(){
+      view.catImgDiv.click(function(){
         var cat = octopus.getCatById($(this).data("id"));
         view.catClickCnt.html(cat.addClickCnt());
         view.totalClickCnt.html(octopus.getTotalClickCnt());
@@ -114,8 +129,8 @@ $(function() {
       var catDom = view.catDiv.data("id", cat.id);
       view.catName.html(cat.name);
       view.catClickCnt.html(cat.clickCnt);
-      view.catImg.data("id", cat.id)
-        .attr("src", cat.imgSrc);
+      view.catImgDiv.data("id", cat.id);
+      view.catImg.attr("src", cat.imgSrc);
     },
 
     makeCatLinkTemplate: function(id) {
